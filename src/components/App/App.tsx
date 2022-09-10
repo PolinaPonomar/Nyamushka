8f6f95170f459b9ce06042678f3df1448cf39675
Polina Ponomareva <pp09117@gmail.com>
2022-09-10 03:25:10 +0300

    feat: add selected logics

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,9 +10,28 @@ const App = ({num}: AppProps) => {
       <div className="content">
         <h1 className="title">Ты сегодня покормил кота?</h1>
         <div className="cards">
-          <Card/>
-          <Card/>
-          <Card/>
+          <Card 
+            taste={'c фуа-гра'}
+            numOfFeeds={'10'}
+            numOfMice={1}
+            numOfKg={'0,5'}
+            selectedCaption={'Печень утки разварная с артишоками.'}
+          />
+          <Card 
+            taste={'c рыбой'}
+            numOfFeeds={'40'}
+            numOfMice={2}
+            numOfKg={'2'}
+            selectedCaption={'Головы щучьи с чесноком да свежайшая сёмгушка.'}
+          />
+          <Card 
+            taste={'c курой'}
+            numOfFeeds={'100'}
+            numOfMice={5}
+            numOfKg={'5'}
+            complimentString={'заказчик доволен'}
+            selectedCaption={'Филе из цыплят с трюфелями в бульоне.'}
+          />
         </div>
       </div>
     </main>
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,33 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Card.scss'
 import { Link } from 'react-router-dom'
 import image from '../../images/cat.png'
 //const image = require('../../images/cat.png') - можно сделать так без index.d.ts и строчек в tsconfig.json, но это нге так красиво (?)
 
-const Card = () => {
+export interface CardProps {
+  taste?: string
+  numOfFeeds: string
+  numOfMice: number
+  numOfKg: string
+  complimentString?: string
+  selectedCaption: string
+}
+
+const Card = (props: CardProps) => {
+  const [isCardSelected, setIsCardSelected] = useState(false);
+
+  function makeСorrectDeclensionOfMouseWord (numOfMice: number): string {
+    if ((numOfMice == 1)||((numOfMice % 10 === 1) && (numOfMice % 100 !== 11))) {
+      return 'мышь'
+    } else if ((numOfMice % 100 > 10) && (numOfMice % 100 < 15)) {
+      return 'мышей'
+    } else if ((numOfMice % 10 > 1) && (numOfMice % 10 < 5)) {
+      return 'мыши'
+    } else {
+      return 'мышей'
+    }
+  }
+
+  function clickOnCard (): void {
+    setIsCardSelected(!isCardSelected)
+  }
+
   return (
     <div className="wrapper">
-      <Link className="card" to="/">
+      <Link className={ isCardSelected ? "card card_selected": "card"} to="/" onClick={clickOnCard}>
         <div className="card__text">
           <p className="card__tagline">Сказочное заморское яство</p>
           <p className="card__title">Нямушка</p>
-          <p className="card__subtitle">с фуа-гра</p>
+          <p className="card__subtitle">{props.taste}</p>
           <p className="card__description">
-            <span className="card__description card__description_accent">10 </span>
+            <span className="card__description card__description_accent">{props.numOfFeeds} </span>
             порций
           </p>
-          <p className="card__description">мышь в подарок</p>
+          <p className="card__description">
+            <span className="card__description card__description_accent">{props.numOfMice == 1 ? '': props.numOfMice} </span>
+            {makeСorrectDeclensionOfMouseWord(props.numOfMice)} в подарок
+          </p>
+          <p className="card__description">{props.complimentString}</p>
         </div>
         <img className="card__photo" src={image} alt='Пушистый котик со светлой шерсткой и миндальными глазами'/>
-        <div className="card__sticker">
+        <div className={ isCardSelected ? "card__sticker card__sticker_selected" :"card__sticker"}>
           <p className="card__sticker-text card__sticker-text_accent">
-            0,5
+            {props.numOfKg}
             <br/>
             <span className="card__sticker-text">кг</span>
           </p>
         </div>
       </Link>
-      <p className="caption">Чего сидишь? Порадуй котэ, <Link className="caption caption_link" to="/">купи.</Link></p>
+      <p className="caption">
+        { isCardSelected ? props.selectedCaption :
+          (<>
+          Чего сидишь? Порадуй котэ,
+          <Link className="caption caption_link" to="/" onClick={clickOnCard}> купи.</Link>
+          </>)
+        }
+      </p>
     </div>
   );
 };
